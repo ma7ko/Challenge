4cf8efa38a5a07c70bf325269457f5d34f7b71b6
Marko Panushkovski <markopanuskovski@gmail.com>
2020-09-08 11:15:44 +0200

    Added migrations on master

---
+++ Challenge/Migrations/202009080910033_eighth.cs
@@ -0,0 +1,40 @@
+namespace Challenge.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class eighth : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.AspNetUsers", "Challenge_Id", "dbo.Challenges");
+            DropIndex("dbo.AspNetUsers", new[] { "Challenge_Id" });
+            CreateTable(
+                "dbo.ApplicationUserChallenges",
+                c => new
+                    {
+                        ApplicationUser_Id = c.String(nullable: false, maxLength: 128),
+                        Challenge_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.ApplicationUser_Id, t.Challenge_Id })
+                .ForeignKey("dbo.AspNetUsers", t => t.ApplicationUser_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Challenges", t => t.Challenge_Id, cascadeDelete: true)
+                .Index(t => t.ApplicationUser_Id)
+                .Index(t => t.Challenge_Id);
+            
+            DropColumn("dbo.AspNetUsers", "Challenge_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "Challenge_Id", c => c.Int());
+            DropForeignKey("dbo.ApplicationUserChallenges", "Challenge_Id", "dbo.Challenges");
+            DropForeignKey("dbo.ApplicationUserChallenges", "ApplicationUser_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.ApplicationUserChallenges", new[] { "Challenge_Id" });
+            DropIndex("dbo.ApplicationUserChallenges", new[] { "ApplicationUser_Id" });
+            DropTable("dbo.ApplicationUserChallenges");
+            CreateIndex("dbo.AspNetUsers", "Challenge_Id");
+            AddForeignKey("dbo.AspNetUsers", "Challenge_Id", "dbo.Challenges", "Id");
+        }
+    }
+}
