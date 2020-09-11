cf9595263fc334cc589ee3289b6c5bb311d10b1f
Marko Panushkovski <markopanuskovski@gmail.com>
2020-09-11 17:06:48 +0200

    User can reupload a photo and when a photo is uploaded a checkmark is being displayed. Moved upload button from details to index

---
+++ Challenge/Migrations/202009101641318_twelfth.cs
@@ -0,0 +1,16 @@
+namespace Challenge.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class twelfth : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
